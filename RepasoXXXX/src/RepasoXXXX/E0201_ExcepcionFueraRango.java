73f0dad53084ff5eb27e57d14c98f562b7991cdf
jmrm <prof.inf@gmail.com>
2013-11-17 01:07:26 +0100

    A ver si sí.

--- RepasoXXXX/src/RepasoXXXX/E0201_ExcepcionFueraRango.java
+++ RepasoXXXX/src/RepasoXXXX/E0201_ExcepcionFueraRango.java
@@ -1,5 +1,10 @@
 package RepasoXXXX;
 public class E0201_ExcepcionFueraRango extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 990720281124365877L;
+
 	public E0201_ExcepcionFueraRango(String msg) {
 		super (msg);
 	}
